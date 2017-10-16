a6a418699aa1815634395e6d960bfbeaf4f9c240
Matt McGrattan <matt.mcgrattan@digirati.com>
2017-10-16 18:15:04 +0100

    Added elapsed time

--- nlw.py
+++ nlw.py
@@ -32,26 +32,27 @@ def images_test(manifest_json, dict_w):
         image_service = canvas['images'][0]['resource']['service']['@id']
         info_json = image_service + '/info.json'
         thumbnail = image_service + '/full/100,/0/native.jpg'
-        tile = image_service + '/0,0,100,100/100,/0/native.jpg'
+        tiles = [image_service + '/0,0,100,100/100,/0/native.jpg', image_service + '/100,100,200,200/100,/0/native.jpg']
         r = requests.get(info_json)
-        result = {'Type': 'Info.json', 'URI': info_json, 'Status': r.status_code}
+        result = {'Type': 'Info.json', 'URI': info_json, 'Status': r.status_code,
+                  'Elapsed Seconds': r.elapsed.total_seconds()}
         print(result)
         dict_w.writerow(result)
         r = requests.get(thumbnail)
         result = {'Type': 'Thumbnail', 'URI': thumbnail, 'Status': r.status_code}
         print(result)
         dict_w.writerow(result)
-        r = requests.get(tile)
-        result = {'Type': 'Tile', 'URI': tile, 'Status': r.status_code}
-        print(result)
-        dict_w.writerow(result)
-
+        for tile in tiles:
+            r = requests.get(tile)
+            result = {'Type': 'Tile', 'URI': tile, 'Status': r.status_code}
+            print(result)
+            dict_w.writerow(result)
 
 
 if __name__ == "__main__":
     manifests = get_manifests_from_file('manifests.txt')
     f = 'output.csv'
-    fieldnames = OrderedDict([('Type', None), ('URI', None), ('Status', None)])
+    fieldnames = OrderedDict([('Type', None), ('URI', None), ('Status', None), ('Elapsed Seconds', None)])
     with open(f, 'wb') as output_csv:
         dw = csv.DictWriter(output_csv, delimiter='\t', fieldnames=fieldnames)
         dw.writeheader()
